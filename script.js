453a4286f663384820b4f5d0c6072d13b0dad51b
Abhinav Jaiswal <114914635+AbhinavJais@users.noreply.github.com>
2023-10-09 13:47:38 +0530

    Updated logic in JS

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ let string = "";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
-        console.log(e.target)
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
-    })
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            Input.value = string;
+        }
+        else if(e.target.innerHTML === 'AC'){
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'DE'){
+            string = string.substring(0,string.length-1)
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
 
+       
+    })
 })
