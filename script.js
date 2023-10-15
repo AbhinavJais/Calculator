e789b11f91a5acb327b5f60f8eeed34a1fffb12d
Abhinav Jaiswal <114914635+AbhinavJais@users.noreply.github.com>
2023-10-15 16:38:05 +0530

    updated JavaScript and HTML

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let string = "";
 let buttons = document.querySelectorAll('.button');
+
+function onlyNumberKey(evt) {
+             
+    // Only ASCII character in that range allowed
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    else{
+        return true;
+    }
+}
+
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            Input.value = string;
+        if(e.target.innerHTML === '='){
+            string = eval(string );
+            document.querySelector('input').value = string;
         }
         else if(e.target.innerHTML === 'AC'){
             string = ""
